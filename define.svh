bd0f4661d6cc22655c2ff9e63fcc80c8648c28cd
RAKSHA NAYAK <138519058+rakshaanayak@users.noreply.github.com>
2025-04-22 14:09:47 +0530

    Update define.svh

--- define.svh
+++ define.svh
@@ -1,3 +1,12 @@
+//------------------------------------------------------------------------------
+// Project      : APB
+// File Name    : define.svh
+// Developer    : Raksha Nayak
+//------------------------------------------------------------------------------
+// Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
+//------------------------------------------------------------------------------
+
+
 `define DW 32
 `define AW 5
 //`define SW int'($ceil(DW/8))
