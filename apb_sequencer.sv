e6b5b26f536930d437dca92ae671e75b64cdc9f9
RAKSHA NAYAK <138519058+rakshaanayak@users.noreply.github.com>
2025-04-22 14:03:41 +0530

    Update apb_sequencer.sv

--- apb_sequencer.sv
+++ apb_sequencer.sv
@@ -1,3 +1,12 @@
+//------------------------------------------------------------------------------
+// Project      : APB
+// File Name    : apb_sequencer.sv
+// Developer    : Raksha Nayak
+//------------------------------------------------------------------------------
+// Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
+//------------------------------------------------------------------------------
+
+
 class apb_sequencer extends uvm_sequencer #(apb_seq_item);
 
   `uvm_component_utils(apb_sequencer)
